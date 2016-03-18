bc789418bc8ce22cf6573e89b8726501e271f8ae
Frank <apps@cloudsense.ca>
2016-03-18 12:03:25 -0400

    First Commit

---
+++ app/src/main/java/com/example/admin/credicardsmgm/MainActivityFragment.java
@@ -0,0 +1,41 @@
+package com.example.admin.credicardsmgm;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar.Tab;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements ActionBar.TabListener {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+
+    }
+}
